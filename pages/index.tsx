c8d4350ee541e99d527e47cbae2abf7332ae6c72
Abeen <abeen@uchicago.edu>
2025-03-13 12:22:11 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -188,12 +188,12 @@ export default function Home() {
 
             {podcast && (
                 <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
-                    <div className="bg-white dark:bg-gray-800 p-6 rounded-lg max-w-2xl w-75 mx-4 shadow-2xl">
+                    <div className="bg-white dark:backgroundDark p-6 rounded-lg max-w-2xl w-75 mx-4 shadow-2xl">
                         <div className="flex justify-between items-center mb-4">
                             <h2 className="text-xl font-bold text-primary">{podcast.title}</h2>
                             <button
                                 onClick={closePodcast}
-                                className="text-gray-500 hover:text-gray-700 dark:hover:text-gray-300"
+                                className="text-gray-500 font-3xl hover:text-gray-700 dark:hover:text-gray-300"
                             >
                                 ✕
                             </button>
