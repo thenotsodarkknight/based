a5dd197146ee916eb16c43abe2be7b21dd83b97f
Abeen <abeen@uchicago.edu>
2025-03-13 03:19:47 -0500

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <title>based</title>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon/favicon.ico?v=1" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+                <meta name="theme-color" content="#ffffff" />
                 <link rel="stylesheet" href="/tailwind.css" />
             </Head>
             <Component {...pageProps} />
